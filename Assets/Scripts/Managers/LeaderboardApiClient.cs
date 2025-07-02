e8346cc7aa49aac56a5d99c9af0976f53f5a7074
Mertalp <32788629+MertalpTasdelen@users.noreply.github.com>
2025-07-02 23:35:41 +0300

    Add API client for global leaderboard and integrate

---
+++ Assets/Scripts/Managers/LeaderboardApiClient.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LeaderboardApiClient : MonoBehaviour
+{
+    public static LeaderboardApiClient Instance;
+
+    [SerializeField] private string apiUrl = "https://example.com/api/scores"; // TODO: change to your API endpoint
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator FetchScores(System.Action<List<GlobalScoreEntry>> onResult)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(apiUrl))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("[LeaderboardApiClient] Failed to fetch scores: " + request.error);
+                onResult?.Invoke(new List<GlobalScoreEntry>());
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+                ScoreListWrapper wrapper = JsonUtility.FromJson<ScoreListWrapper>(json);
+                if (wrapper != null && wrapper.scores != null)
+                    onResult?.Invoke(wrapper.scores);
+                else
+                    onResult?.Invoke(new List<GlobalScoreEntry>());
+            }
+        }
+    }
+
+    [System.Serializable]
+    private class ScoreListWrapper
+    {
+        public List<GlobalScoreEntry> scores;
+    }
+}
